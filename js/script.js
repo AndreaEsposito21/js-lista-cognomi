a82e7b37f3f67b8201db753b819bb22f533d7194
Andrea Esposito <andexpox@gmail.com>
2021-03-24 20:18:49 +0100

    for-while

--- js/script.js
+++ js/script.js
@@ -24,12 +24,39 @@ for ( var i = 0; i < listaCognomi.length; i++) {
 }
 console.log('alfabeticamente ordinata', listaCognomi);
 
-//Posizione utente - Versione For
+//Posizione utente:
+var posizioneUtente = -1;
 
-for ( i = 0; i < listaCognomi.length; i++) {
 
+// - Versione For ------------------------- - Da scommentare per provare -
+// for ( i = 0; i < listaCognomi.length; i++) {
     
+//     if (cognomeUtente == listaCognomi[i]) {
+        
+//         posizioneUtente = i + 1;
+//     }
+    
+// }
+
+
+// - Versione While ------------------------- - Da scommentare per provare -
+// var i = 0;
+
+// while ( i < listaCognomi.length ) {
+  
+//     if (cognomeUtente == listaCognomi[i]) {
+        
+//         posizioneUtente = i + 1;
+//     }
+
+//     i++;
+// }
+
+// Numero Posizione Utente
+if ( posizioneUtente == -1 ) {
+    alert("Utente non trovato");
+} else {
+    alert("Sei il numero: " + posizioneUtente);
 }
 
-
-//Posizione utente - Versione While
+console.log("Sei il numero: " + posizioneUtente);
